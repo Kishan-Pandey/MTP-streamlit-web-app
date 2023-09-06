96ab5ada459a9d14eb12983bd5017b77f72f8115
Kishan-Pandey <77564344+Kishan-Pandey@users.noreply.github.com>
2023-09-06 12:44:47 +0530

    Add files via upload

---
+++ Web-Application.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+st.sidebar.success("Select a demo above.")
+
+st.markdown("<h1 style='text-align: center;'>Automated Classification and Detection of Pavement Surface Condition using Deep Learning</h1>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>Thesis to be submitted in fulfillment of the requirements for the degree of</h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>M.Tech Project - II</h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>by</h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'><b>Aditya R. Chakole</b></h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>18CE3AI15</h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>Under the guidance of <b>Dr. Kranthi Kumar Kuna</b></h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>Department of Civil Engineering, with specialization in Artificial Intelligence, Machine Learning And Applications</h3>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>INDIAN INSTITUTE OF TECHNOLOGY KHARAGPUR</h3>", unsafe_allow_html=True)
